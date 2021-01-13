8c17d8729ab2343491aecebed5235b29240f35fa
Leticia Pillar Lisboa <leticiapillar@gmail.com>
2021-01-13 07:50:29 -0300

    Microsserviço: Catálogo de vídeos com Laravel (Back-end)
      - Exclusão Lógica
      - alteração na classe de migração CreateCategoriesTable, para usar o softDeletes
      - habilitando o SoftDeletes na classe Model de Category
      - configurando o campo deleted_at para ser do tipo date
      - documentação dos comandos no readme

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
+    use SoftDeletes;
+
     // estes campos são utilizados no POST ao criar uma categoria
     protected $fillable = ['name', 'description', 'is_active'];
+    // entende que o campos deletec_at é do tipo data
+    protected $dates = ['deleted_at'];
 }
