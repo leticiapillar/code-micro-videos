4260a96744d19020ecc24f315c098a31d3b01a8f
Leticia Pillar Lisboa <leticiapillar@gmail.com>
2021-01-13 08:26:44 -0300

    Microsserviço: Catálogo de vídeos com Laravel (Back-end)
      - Substituindo ID Por UUID
      - alteração na classe de migração CreateCategoriesTable, para o id ser do tipo uuid
      - implementação da trait Uuid para gerar um uuid para a aplicação
      - alteração na model Category para utilizar o uuid ao criar uma categoria

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,15 +2,31 @@
 
 namespace App\Models;
 
+use App\Models\Traits\Uuid;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, Uuid;
 
     // estes campos são utilizados no POST ao criar uma categoria
     protected $fillable = ['name', 'description', 'is_active'];
     // entende que o campos deletec_at é do tipo data
     protected $dates = ['deleted_at'];
+
+    // servem par informar que não é auto-incremento no banco e que a chave primária é string
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    // este passou para um trait para ser reutilizado
+    // // evento creating, sobrescreve o evento boot para setar o uuid ao criar uma categoria
+    // public static function boot()
+    // {
+    //     parent::boot();
+    //     static::creating(function($obj){
+    //         $obj->id = Uuid::uuid4();
+    //     });
+    // }
+
 }
