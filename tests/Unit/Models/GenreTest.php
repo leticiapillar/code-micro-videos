e8a7de255e6b1b546e89afa116f09918b8a8ce15
Leticia Pillar Lisboa <leticiapillar@gmail.com>
2021-01-26 06:03:08 -0300

    Microsserviço: Catálogo de vídeos com Laravel (Back-end)
      - Testes de integração em categorias e gêneros
      - inclusão dos testes integração para id e delete em CategoryTest
      - implementação do teste de unidade para Genre
      - implementação do teste de integração para Genre

---
+++ tests/Unit/Models/GenreTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\Genre;
+use App\Models\Traits\Uuid;
+use PHPUnit\Framework\TestCase;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class GenreTest extends TestCase
+{
+    private $genre;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->genre = new Genre();
+    }
+    public function testIfUseTraits()
+    {
+        $traits = [
+            SoftDeletes::class, Uuid::class
+        ];
+        $genreTraits = array_keys(class_uses(Genre::class));
+        $this->assertEquals($traits, $genreTraits);
+    }
+
+    public function testFillableAttributes()
+    {
+        $fillable = ['name', 'is_active'];
+        $this->assertEquals($fillable, $this->genre->getFillable());
+    }
+
+    public function testCastsAttributes()
+    {
+        $casts = ['is_active' => 'boolean'];
+        $this->assertEquals($casts, $this->genre->getCasts());
+    }
+
+    public function testKeyTypeAttribule()
+    {
+        $this->assertEquals('string', $this->genre->getKeyType());
+    }
+
+    public function testIncrementingAttribute()
+    {
+        $this->assertFalse($this->genre->incrementing);
+    }
+
+    public function testDateAttributes()
+    {
+        $dates = ['deleted_at', 'created_at', 'updated_at'];
+        $this->assertEqualsCanonicalizing($dates, $this->genre->getDates());
+        $this->assertCount(count($dates), $this->genre->getDates());
+    }
+
+}
