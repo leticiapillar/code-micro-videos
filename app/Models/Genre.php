2b8b92332666ef07b85509583bbd46b0df30349a
Leticia Pillar Lisboa <leticiapillar@gmail.com>
2021-01-28 17:57:47 -0300

    Microsserviço: Catálogo de vídeos com Laravel (Back-end)
      - Testes com HTTP, testando a CategoryController
      - Configurando CI no GCP
      - ajuste no arquivo composer para sobrescrever o arquivo _ide_helper_models.php
      - exclusao de arquivos desnecessarios
      - ajustes nos arquivos cloudbuild e docker-compose

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -6,6 +6,29 @@ use App\Models\Traits\Uuid;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * App\Models\Genre
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Genre newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Genre newQuery()
+ * @method static \Illuminate\Database\Query\Builder|Genre onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Genre query()
+ * @method static \Illuminate\Database\Query\Builder|Genre withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|Genre withoutTrashed()
+ * @mixin \Eloquent
+ * @property string $id
+ * @property string $name
+ * @property bool $is_active
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Genre whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Genre whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Genre whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Genre whereIsActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Genre whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Genre whereUpdatedAt($value)
+ */
 class Genre extends Model
 {
     use SoftDeletes, Uuid;
