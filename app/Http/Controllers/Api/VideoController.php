8af7cd5be1cc116eb5e09a4727347106ed160fa6
Leticia Pillar Lisboa <leticiapillar@gmail.com>
2021-02-09 07:52:54 -0300

    Microsserviço: Catálogo de vídeos com Laravel (Back-end)
      - Iniciando recursos de vídeo
      - Testando Crud de vídeo
      - implementação dos testes na controller de Video

--- app/Http/Controllers/Api/VideoController.php
+++ app/Http/Controllers/Api/VideoController.php
@@ -6,7 +6,19 @@ use App\Models\Video;
 
 class VideoController extends BasicCrudController
 {
-    private $rules = [];
+    private $rules;
+
+    public function __construct()
+    {
+        $this->rules = [
+            'title' => 'required|max:255',
+            'description' => 'required',
+            'year_lauched' => 'required|date_format:Y',
+            'opened' => 'boolean',
+            'rating' => 'required|in:' . implode(',', Video::RATING_LIST),
+            'duration' => 'required|integer'
+        ];
+    }
 
     public function model()
     {
