1e253306c313b6061f2a700f124e1d85b6000964
Leticia Pillar Lisboa <leticiapillar@gmail.com>
2021-03-22 07:38:56 -0300

    Microsserviço: Catálogo de vídeos com Laravel (Back-end)
      - Upload de arquivos - Parte2
      - Primeiro upload de arquivo
      -  Separando testes de CRUD e uploads para o vídeo
      - refatoração nos testes de integração do Video

---
+++ tests/Feature/Models/Video/BaseVideoTestCase.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature\Models\Video;
+
+use App\Models\Video;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+abstract class BaseVideoTestcase extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected $sendData;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->sendData = [
+            'title' => 'title test',
+            'description' => 'description test',
+            'year_lauched' => 2010,
+            'rating' => Video::RATING_LIST[0],
+            'duration' => 90
+        ];
+    }
+}
--- tests/Feature/Models/Video/VideoCrudTest.php
+++ tests/Feature/Models/Video/VideoCrudTest.php
@@ -1,32 +1,14 @@
 <?php
 
-namespace Tests\Feature\Models;
+namespace Tests\Feature\Models\Video;
 
 use App\Models\Category;
 use App\Models\Genre;
 use App\Models\Video;
 use Illuminate\Database\QueryException;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Tests\TestCase;
 
-class VideoTest extends TestCase
+class VideoCrudTest extends BaseVideoTestcase
 {
-    use DatabaseMigrations;
-
-    private $sendData;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-        $this->sendData = [
-            'title' => 'title test',
-            'description' => 'description test',
-            'year_lauched' => 2010,
-            //'opened' => false,
-            'rating' => Video::RATING_LIST[0],
-            'duration' => 90
-        ];
-    }
 
     public function testList()
     {
